7092087096d43c3c284fdd9430e7948b2835fafd
Zeeshan Qureshi <zee@zqureshi.in>
2017-09-24 16:42:45 -0400

    Refactor into RedirectClient

---
+++ airtable.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fabioberger/airtable-go"
+)
+
+const redirectTableName = "Redirects"
+const defaultRedirectKey = "default"
+
+type RedirectClient struct {
+	airtableGo *airtable.Client
+}
+
+type Redirect struct {
+	Key string
+	URL string
+}
+
+func NewRedirectClient(apiKey string, baseId string) (*RedirectClient, error) {
+	airtableGo, err := airtable.New(apiKey, baseId)
+	return &RedirectClient{airtableGo: airtableGo}, err
+}
+
+func (client *RedirectClient) GetRedirect(key string) (*Redirect, error) {
+	records := []struct {
+		AirtableID string
+		Fields     Redirect
+	}{}
+
+	err := client.airtableGo.ListRecords(
+		redirectTableName,
+		&records,
+		airtable.ListParameters{FilterByFormula: fmt.Sprintf("{Key} = '%s'", key), MaxRecords: 1},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &records[0].Fields, nil
+}
+
+func (client *RedirectClient) GetDefault() (*Redirect, error) {
+	return client.GetRedirect(defaultRedirectKey)
+}
--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"github.com/fabioberger/airtable-go"
 	"log"
 	"net/http"
 	"os"
 )
 
 var (
-	airtableApiKey    = os.Getenv("AIRTABLE_API_KEY")
-	airtableBaseId    = os.Getenv("AIRTABLE_BASE_ID")
-	airtableTableName = "Redirects"
+	airtableApiKey = os.Getenv("AIRTABLE_API_KEY")
+	airtableBaseId = os.Getenv("AIRTABLE_BASE_ID")
 )
-
-type Redirect struct {
-	Key string
-	URL string
-}
-
-type Record struct {
-	AirtableID string
-	Fields     Redirect
-}
 
 func main() {
 	if airtableApiKey == "" {
@@ -32,27 +20,15 @@ func main() {
 		panic("AIRTABLE_BASE_ID must be specified")
 	}
 
-	client, err := airtable.New(airtableApiKey, airtableBaseId)
+	client, err := NewRedirectClient(airtableApiKey, airtableBaseId)
 	if err != nil {
 		panic(err)
 	}
 
-	records := []Record{}
-	err = client.ListRecords(
-		airtableTableName,
-		&records,
-		airtable.ListParameters{FilterByFormula: "{Key} = 'default'", MaxRecords: 1},
-	)
-
+	defaultRedirect, err := client.GetDefault()
 	if err != nil {
-		panic(err)
-	}
-
-	if len(records) != 1 {
 		panic("A 'default' redirect must be specified")
 	}
-
-	defaultRedirect := records[0].Fields
 	log.Println("Default redirect " + defaultRedirect.URL)
 
 	http.HandleFunc("/_ah/health", func(w http.ResponseWriter, _ *http.Request) {
